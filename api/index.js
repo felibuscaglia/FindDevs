560a93279f8f24d303e0498308bdf9eb5f5996ff
felibuscaglia <felipebenjamin1810@gmail.com>
2021-03-03 16:01:26 -0300

    Heroku deploy

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
+const PORT = process.env.PORT || 5001;
+
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log(`%s listening at ${process.env.PORT}`); 
+  server.listen((PORT, '0.0.0.0'), () => {
+    console.log(`%s listening at ${PORT}`); 
   });
 });
