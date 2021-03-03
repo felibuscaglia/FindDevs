d49a316c978f8485bd068ef8baec19232a717fdb
felibuscaglia <felipebenjamin1810@gmail.com>
2021-03-03 17:18:13 -0300

    Final commit

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const { conn } = require("./src/db.js");
 const PORT = process.env.PORT || 5001;
 
 conn.sync({ force: false }).then(() => {
-  server.listen((PORT, '0.0.0.0'), () => {
+  server.listen(PORT, () => {
     console.log(`%s listening at ${PORT}`); 
   });
 });
